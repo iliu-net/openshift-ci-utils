f7bd3200f9439bc999a4619d490fe12bfe74bb02
alejandroliu <alejandro_liu@hotmail.com>
2016-07-12 15:53:08 +0200

    Initial import

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# openshift-ci-utils
-Scripts that I used to integrate with openshift
+# openshift-utils
+
+Scripts that I used to integrate with openshift.
+
+## deploy.sh
+
+This is what I used to deploy to openshift.  I use it because
+the built-in `dpl` script in `travis-ci` works a bit weird
+for my taste.
+
+### Configuring
+
+0. Pre-requisites:
+   - Install the travis command-line client.
+1. Add this repo to your OpenShift project (as a submodule).
+   - git submodule add $repourl $dir
+1. When cloning your project repo use:
+   - git clone --recursive $project
+2. Create/modify a `.travis.yml` file.
+   - Add your tests as necessary
+   - Configure your openshift credentials:
+     - travis encrypt OPENSHIFT_USER=$username --add env.global
+     - travis encrypt OPENSHIFT_SECRET=$pwd --add env.global
+   - In a section `diydeploy:` (actually can be anything), add lines like:
+     - {space}-{space}deploy{space}$branch:$openshift_app
+   - Create deploy section.  See example:
+
+```yaml
+    # Example deploy
+    deploy:
+      provider: script
+      script: ./$dir/deploy.sh
+      on:
+        all_branches: true
+```
+
+## NOTES
+
+- travis has a number of places to run stuff:
+  - `before_install`, `install`, `before_script` : these run
+    before the test script and stops immediatly when an error occurs.
+    Any errors here will cause the build as being marked **failed**.
+  - `script` : commands here can return non-zero and that marks the build
+    as failed.  But it will not stop the script from executing.
+  - `after_success`, `after_failure`, `after_script` : these run after
+    `script` and its results do not affect the status of the build.
+  - `deploy` : Can be overriden with _experimental_ script provider.
+  - `before_deploy` can mark the build as **errored**.
---
+++ deploy.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+#
+fatal() {
+  echo "$@" 1>&2
+  exit 1
+}
+
+echo "DIY DEPLOY OPENSHIFT"
+if [ -n "$TRAVIS_BRANCH" ] ; then
+  OPENSHIFT_APP="$(grep ' - deploy '"$TRAVIS_BRANCH"':' .travis.yml | cut -d: -f2)"
+  if [ -n "$OPENSHIFT_APP" ] ; then
+    echo "App: $OPENSHIFT_APP"
+  else
+    echo "Branch $TRAVIS_BRANCH is not deployable"
+    exit 0
+  fi
+else
+  echo "Not a BRANCH commit.  No deployment possible"
+  exit 0
+fi
+[ -n "$OPENSHIFT_USER" ] && echo "USER: $OPENSHIFT_USER"
+if [ -n "$OPENSHIFT_SECRET" ] ; then
+  echo -n "SECRET: "
+  cnt=$(expr length "$OPENSHIFT_SECRET")
+  while [ $cnt -gt 0 ]
+  do
+    echo -n "*"
+    cnt=$(expr $cnt - 1)
+  done
+  echo ""
+fi
+[ -n "$TRAVIS_BRANCH" ] && echo "Branch: $TRAVIS_BRANCH"
+[ -n "$TRAVIS_PULL_REQUEST" ] && echo "PR: $TRAVIS_PULL_REQUEST"
+[ -n "$TRAVIS_REPO_SLUG" ] && echo "Slug: $TRAVIS_REPO_SLUG"
+[ -n "$TRAVIS_TAG" ] && echo "Tag: $TRAVIS_TAG"
+
+for k in OPENSHIFT_USER OPENSHIFT_SECRET OPENSHIFT_APP
+do
+  eval v="\$$k"
+  [ -z "$v" ] && fatal "MISSING $k"
+done
+#
+set -e
+gem install rhc
+AUTH="-l $OPENSHIFT_USER -p $OPENSHIFT_SECRET"
+rhc app-show $OPENSHIFT_APP $AUTH | grep -v 'Password:' | grep -v 'Username:'
+GITURL="$(rhc app-show $OPENSHIFT_APP $AUTH| grep '  Git URL: ' | cut -d: -f2-)"
+[ -z "$GITURL" ] && fatal "MISSING GITURL"
+GITHOST="$(echo $GITURL | cut -d'@' -f2 | cut -d/ -f1)"
+[ -z "$GITHOST" ] && fatal "MISSING GITHOST"
+ssh-keyscan $GITHOST > ~/.ssh/known_hosts
+
+yes '' | ssh-keygen -N ''
+rhc sshkey remove temp $AUTH || true
+rhc sshkey add temp $HOME/.ssh/id_rsa.pub $AUTH
+git remote add openshift -f $GITURL
+pwd
+git status
+git remote -v
+echo Merging changes from openshift/master to our branch
+git merge openshift/master -s recursive -X ours
+echo Sending changes to openshift/master
+git push openshift HEAD:master
+rhc sshkey remove temp $AUTH || true
